da5f33494f3e4e0671cbe1700b1fe968d0f3ef2c
volekji <jindra.volek@gmail.com>
2024-02-27 16:19:14 +0100

    Add game captions

--- app/src/main/java/com/example/mosaicmind/GameActivity.kt
+++ app/src/main/java/com/example/mosaicmind/GameActivity.kt
@@ -2,23 +2,29 @@ package com.example.mosaicmind
 
 import android.content.Intent
 import android.graphics.Color
-import android.icu.lang.UCharacter.VerticalOrientation
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
+import org.json.JSONArray
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
 
     private var difficulty: String? = null
     private var size: Int = 5
     private var lives: Int = 3
-    private lateinit var board: Array<BooleanArray>
     private var remainingCells: Int = 0
+    private var colorMode: Boolean = true
+    private lateinit var currentBoard: Array<BooleanArray>
+    private lateinit var finishedBoard: Array<BooleanArray>
     private lateinit var heart1: ImageView
     private lateinit var heart2: ImageView
     private lateinit var heart3: ImageView
@@ -37,11 +43,15 @@ class GameActivity : AppCompatActivity() {
         val difficultyTextView: TextView = findViewById(R.id.difficultyTextView)
         difficultyTextView.text = "Selected Difficulty: $difficulty"
         */
-        board = Array(size) { BooleanArray(size) }
+        finishedBoard = Array(size) { BooleanArray(size) }
+        currentBoard = Array(size) { BooleanArray(size) }
         initializeGame()
 
         val backButton: ImageButton = findViewById(R.id.backButton)
         backButton.setOnClickListener { view -> goBack(view) }
+
+        val changeModeButton: Button = findViewById(R.id.changeModeButton)
+        changeModeButton.setOnClickListener { view -> changeColorMode(view) }
 
         heart1 = findViewById(R.id.heart1)
         heart2 = findViewById(R.id.heart2)
@@ -73,14 +83,114 @@ class GameActivity : AppCompatActivity() {
     }
 
     private fun loadBoard(size: Int) {
-        //TODO Load board from a file:
-        // Initialize board array
-        // Initialize NOF good cells and their caption to to View
-        for(i in 0..<size) {
-            for(j in 0..<size) {
-                if((j % 2 == 0)) remainingCells++
-                board[i][j] = (j % 2 == 0)
+        val gameResource = when(difficulty) {
+            "Easy" -> R.raw.easy
+            "Medium" -> R.raw.easy
+            "Difficult" -> R.raw.easy
+            else -> R.raw.easy
+        }
+
+        val boardArray = getBoardFromResource(gameResource)
+
+        for(rowNumber in 0..<size) {
+            val row = boardArray.getJSONArray(rowNumber)
+            for (columnNumber in 0..<size) {
+                val cell = row.getInt(columnNumber) == 1
+                if (cell) remainingCells++
+                finishedBoard[rowNumber][columnNumber] = cell
             }
+        }
+
+        setBoardCaptions()
+    }
+
+    private fun getBoardFromResource(resource: Int) : JSONArray {
+        val jsonText = resources.openRawResource(resource)
+            .bufferedReader().use { it.readText() }
+
+        val boardsArray = JSONArray(jsonText)
+        val boardObject = boardsArray.getJSONObject(Random.Default.nextInt(0, boardsArray.length()))
+
+        return boardObject.getJSONArray("board")
+    }
+
+    private fun createCaption(caption:String) : TextView {
+        val captionView = TextView(this)
+        captionView.text = caption
+        captionView.isSingleLine = true
+        captionView.textSize = 22F
+        captionView.setTypeface(null, Typeface.BOLD)
+        captionView.gravity = Gravity.CENTER
+        captionView.setTextColor(Color.LTGRAY)
+        return captionView
+    }
+    private fun setBoardCaptions() {
+        var captions = mutableListOf<String>()
+        val rowCaptions: LinearLayout = findViewById(R.id.rowCaptions)
+        val columnCaptions: LinearLayout = findViewById(R.id.columnsCaptions)
+
+        //Count rows
+        for(rowNumber in 0..<size) {
+            var blockSize = 0
+            for (columnNumber in 0..<size) {
+                if(!finishedBoard[rowNumber][columnNumber]) {
+                    if(blockSize != 0) {
+                        captions.add(blockSize.toString())
+                    }
+                    blockSize = 0
+                }
+                else {
+                    blockSize++
+                }
+            }
+
+            if(blockSize != 0) {
+                captions.add(blockSize.toString())
+            }
+            var text = ""
+            for(i in 0..<captions.size) {
+                text += captions[i]
+                if(i != captions.size - 1) {
+                    text += ","
+                }
+            }
+
+            rowCaptions.addView(createCaption(text),
+                LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT,1F))
+            captions = mutableListOf()
+        }
+
+        //Count columns
+        for(columnNumber in 0..<size) {
+            var blockSize = 0
+            for (rowNumber in 0..<size) {
+                if(!finishedBoard[rowNumber][columnNumber]) {
+                    if(blockSize != 0) {
+                        captions.add(blockSize.toString())
+                    }
+                    blockSize = 0
+                }
+                else {
+                    blockSize++
+                }
+            }
+
+            if(blockSize != 0) {
+                captions.add(blockSize.toString())
+            }
+
+
+            var text = ""
+            for(i in 0..<captions.size) {
+                text += captions[i]
+                if(i != captions.size - 1) {
+                    text += ","
+                }
+            }
+            columnCaptions.addView(createCaption(text),
+                LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT,1F))
+
+            captions = mutableListOf()
         }
     }
 
@@ -88,20 +198,50 @@ class GameActivity : AppCompatActivity() {
         button.isClickable = false
         val rowNumber: Int = button.id / 100
         val columnNumber: Int = button.id % 100
-        if (board[rowNumber][columnNumber]) {
-            remainingCells--
-            button.setBackgroundColor(Color.DKGRAY)
+        if(colorMode) {
+            if (finishedBoard[rowNumber][columnNumber]) {
+                remainingCells--
+
+                button.setBackgroundColor(Color.DKGRAY)
+            } else {
+                button.setBackgroundColor(Color.RED)
+                lives--
+                updateHearts()
+            }
         } else {
-            button.setBackgroundColor(Color.RED)
-            lives--
-            updateHearts()
+            if (!finishedBoard[rowNumber][columnNumber]) {
+                button.setBackgroundColor(Color.RED)
+            } else {
+                button.setBackgroundColor(Color.DKGRAY)
+                remainingCells--
+                lives--
+                updateHearts()
+            }
         }
 
+
         checkGameStatus()
+
+        fillCompletedRows()
+        //fillCompletedColumns()
+
     }
 
+    private fun fillCompletedRows() {
 
-    fun checkGameStatus() {
+    }
+
+    private fun changeColorMode(view: View) {
+        colorMode = !colorMode
+        val button : Button = view as Button
+        if(colorMode) {
+            button.text = "Color"
+        } else {
+            button.text = "Cross"
+        }
+    }
+
+    private fun checkGameStatus() {
         if(lives <= 0) {
             //TODO Show "You lose" message
             finish()
@@ -119,6 +259,7 @@ class GameActivity : AppCompatActivity() {
             0 -> heart1.setImageResource(R.drawable.heart_missing)
         }
     }
+
 
     fun goBack(view: View) {
         val intent = Intent(this, MainActivity::class.java)
